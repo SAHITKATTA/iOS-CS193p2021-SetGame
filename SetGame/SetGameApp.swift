31480966e749b447a6e3e68afecc0fc76f048a11
SahitKatta <sahit.mohan@gmail.com>
2022-02-27 10:32:55 +0530

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Sanjay Katta on 27/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SetGameView()
+        }
+    }
+}
